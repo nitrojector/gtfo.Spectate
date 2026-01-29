774ea25c065bf11d945df203747b94d0d3ecdb7a
wyvern <nitrojector@takina.io>
2026-01-29 04:19:49 -0800

    feat: improve TMP UI positioning

--- Spectate/SpectateUI.cs
+++ Spectate/SpectateUI.cs
@@ -7,8 +7,11 @@ namespace Spectate;
 public class SpectateUI : MonoBehaviour {
 	public static SpectateUI? Instance { get; private set; }
 
+	private bool _wasUIActive = false;
+
 	private const string SpectateTargetTextName = $"{Plugin.GUID}_SpectateTargetText";
 	private TextMeshPro? _specTargetTmp;
+	private readonly string _specTargetTextColor = "FFFFFF";
 
 	public SpectateUI(IntPtr ptr) : base(ptr) {
 	}
@@ -29,12 +32,14 @@ public class SpectateUI : MonoBehaviour {
 
 		_specTargetTmp = Instantiate(pstatus.m_healthText);
 		_specTargetTmp.alignment = TextAlignmentOptions.Center;
-		_specTargetTmp.verticalAlignment = VerticalAlignmentOptions.Top;
 		_specTargetTmp.name = SpectateTargetTextName;
-		_specTargetTmp.fontSize = 18f;
+		_specTargetTmp.fontSize = 26f;
+		_specTargetTmp.color = new(1.0f, 1.0f, 1.0f, 0.8f);
 		_specTargetTmp.enableAutoSizing = false;
-		_specTargetTmp.transform.SetParent(pstatus.transform);
-		_specTargetTmp.transform.localPosition = new Vector3(0f, -12f, 0f);
+		_specTargetTmp.rectTransform.SetParent(pstatus.transform.parent); // set parent to MovementRoot
+		_specTargetTmp.rectTransform.anchorMin = new Vector2(0.5f, 1.0f);
+		_specTargetTmp.rectTransform.anchorMax = new Vector2(0.5f, 1.0f);
+		_specTargetTmp.rectTransform.anchoredPosition = new Vector2(0f, -200f);
 		_specTargetTmp.gameObject.SetActive(false);
 		return true;
 	}
@@ -42,29 +47,35 @@ public class SpectateUI : MonoBehaviour {
 	private void Update() {
 		if ((!SpectateCam.Instance?.Active ?? true) ||
 		    !SpectateCam.Instance.TargetReady) {
-			if (_specTargetTmp != null && _specTargetTmp.gameObject.activeSelf) {
-				_specTargetTmp.gameObject.SetActive(false);
-			}
+			SetUIActive(false);
 
 			return;
 		}
 
-		if (!CheckOrUpdateTMP()) return;
-		if (!(_specTargetTmp!.gameObject.activeSelf)) {
-			_specTargetTmp!.gameObject.SetActive(true);
-		}
+		if (!CheckOrUpdateTMP()) return; // ensure TMP is valid
+		SetUIActive(true);
 
 		string playerName = SpectateCam.Instance.Target?.Agent.PlayerName ?? "<#FFDE21>Unknown</color>";
-		string spectateText = $"Spectating\n{playerName}";
+		string spectateText = $"<#{_specTargetTextColor}>Spectating\n<size=36>{playerName}</size></color>";
 		if (spectateText != _specTargetTmp!.text) {
 			_specTargetTmp!.text = spectateText;
-			ForceTMPUpdate();
+			ForceTMPUpdate(_specTargetTmp);
 		}
 	}
 
-	void ForceTMPUpdate() {
-		if (_specTargetTmp == null) return;
-		_specTargetTmp.SetAllDirty();
-		_specTargetTmp.ForceMeshUpdate();
+	void SetUIActive(bool active) {
+		if (_wasUIActive == active) return;
+		Util.SetObjActiveIfChanged(_specTargetTmp, active);
+		_wasUIActive = active;
+	}
+
+	void ForceTMPUpdate(TextMeshPro tmp) {
+		if (tmp == null) return;
+		tmp.SetAllDirty();
+		tmp.ForceMeshUpdate();
+	}
+
+	public void WarnFreecamNoAdjustPitch() {
+		// TODO:
 	}
 }
---
+++ Spectate/Util.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Spectate;
+
+public static class Util {
+	public const float GOOD_ENOUGH_EPS = 0.004f;
+
+	public static bool GoodEnough(float a, float b) {
+		return NearlyEqual(a, b, GOOD_ENOUGH_EPS);
+	}
+
+	public static bool NearlyEqual(float a, float b, float eps = 0.0001f) {
+		return Mathf.Abs(a - b) < eps;
+	}
+
+	public static void SetObjActiveIfChanged(GameObject? obj, bool active) {
+		if (obj == null) return;
+		if (obj.activeSelf != active) {
+			obj.SetActive(active);
+		}
+	}
+
+	public static void SetObjActiveIfChanged(Component? comp, bool active) {
+		if (comp == null) return;
+		if (comp.gameObject.activeSelf != active) {
+			comp.gameObject.SetActive(active);
+		}
+	}
+}
